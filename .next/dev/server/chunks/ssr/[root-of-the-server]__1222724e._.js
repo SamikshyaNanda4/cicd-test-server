23f01523e0fe3d1f847f2261f2f93bba8002a6d3
samikshyananda4 <samikshya.nanda.4848@gmail.com>
2026-01-26 16:09:22 +0530

    prod build: 2026-01-26 16:09:22

--- .next/dev/server/chunks/ssr/[root-of-the-server]__1222724e._.js
+++ .next/dev/server/chunks/ssr/[root-of-the-server]__1222724e._.js
@@ -132,7 +132,7 @@ function Home() {
                     }, this),
                     /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("div", {
                         className: "inline-block bg-[#000080] text-white px-6 py-2 rounded-full text-xl font-semibold mb-6",
-                        children: "76th Republic Day - 2025"
+                        children: "77th Republic Day - 2026"
                     }, void 0, false, {
                         fileName: "[project]/app/page.tsx",
                         lineNumber: 38,
@@ -140,7 +140,7 @@ function Home() {
                     }, this),
                     /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("p", {
                         className: "text-2xl md:text-3xl text-gray-700 mb-4",
-                        children: "Jai Hind!"
+                        children: "Jai Hind! Bo Pile"
                     }, void 0, false, {
                         fileName: "[project]/app/page.tsx",
                         lineNumber: 43,
@@ -167,7 +167,7 @@ function Home() {
                             }, this),
                             /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("p", {
                                 className: "text-2xl font-bold bg-gradient-to-r from-[#FF9933] via-[#000080] to-[#138808] bg-clip-text text-transparent",
-                                children: "Samikshya Nanda"
+                                children: "Samikshya Nanda & Pratik"
                             }, void 0, false, {
                                 fileName: "[project]/app/page.tsx",
                                 lineNumber: 58,
